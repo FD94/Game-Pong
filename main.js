c313cf00b217a1edef733c1fee1ad392595d641b
Gianfranco <gianfranco.devita@hotmail.com>
2026-03-02 17:58:34 +0100

    feat: create the global variable

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+const arena = document.getElementById("arena");
+const ballEl = document.getElementById("ball");
+const paddleLeftEl = document.querySelector(".paddle-left");
+const paddleRightEl = document.querySelector(".paddle-right");
+const scoreLeftEl = document.getElementById("score-left");
+const scoreRightEl = document.getElementById("score-right");
