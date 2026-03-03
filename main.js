6304793de9b528ca85e0c9a2ada6ca6b4d28a780
Gianfranco <gianfranco.devita@hotmail.com>
2026-03-03 14:52:01 +0100

    Implement movement to paddles

--- main.js
+++ main.js
@@ -1,6 +1,56 @@
-const arena = document.getElementById("arena");
-const ballEl = document.getElementById("ball");
-const paddleLeftEl = document.querySelector(".paddle-left");
-const paddleRightEl = document.querySelector(".paddle-right");
-const scoreLeftEl = document.getElementById("score-left");
-const scoreRightEl = document.getElementById("score-right");
+const ballElement = document.getElementById("ball");
+const playerElement = document.getElementById("player");
+const machineElement = document.getElementById("machine");
+
+class Paddle {
+	constructor(domElement, isPlayer = false) {
+		this.domElement = domElement;
+		this.isPlayer = isPlayer;
+		this.positionX = this.isPlayer ? 0.1 : 48.5;
+		this.heigth = 6;
+		this.width = 1;
+		this.speed = 3;
+		this.positionY = 0 - this.width / 2;
+
+		this.update();
+	}
+	moveUp() {
+		this.positionY--;
+		this.update();
+	}
+	moveDown() {
+		this.positionY++;
+		this.update();
+	}
+	update() {
+		this.domElement.style.top = this.positionY + "vh";
+		this.domElement.style.bottom = this.positionY + "vh";
+		this.domElement.style.width = this.width + "vw";
+		this.domElement.style.height = this.heigth + "vw";
+	}
+}
+
+let direction = 1;
+
+function moveMachine() {
+	machine.positionY += direction;
+
+	if (machine.positionY <= 0 || machine.positionY >= 50) {
+		direction *= -1;
+	}
+
+	machine.update();
+}
+
+setInterval(moveMachine, 30);
+
+const player = new Paddle(playerElement, true);
+const machine = new Paddle(machineElement, false);
+
+document.addEventListener("keydown", (e) => {
+	if (e.code === "ArrowUp") {
+		player.moveUp();
+	} else if (e.code === "ArrowDown") {
+		player.moveDown();
+	}
+});
