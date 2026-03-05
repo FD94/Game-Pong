b808e7358da57183029f99647f8107e16392474a
Gianfranco <gianfranco.devita@hotmail.com>
2026-03-05 12:42:25 +0100

    feat: Add style to instruction and fix: relative path

--- gameLogic/main.js
+++ gameLogic/main.js
@@ -175,6 +175,7 @@ function checkWinningScore() {
 
 	if (playerScore >= winningScore || machineScore >= winningScore) {
 		alert(playerScore >= winningScore ? "¡Ganaste!" : "La máquina ganó!");
+		location.href = "../gameLogic/instruction.js";
 		// Reinicia los puntajes
 		playerScore = 0;
 		machineScore = 0;
